8c11e8697d9d7a0a28a488c36ce5a204367346d8
vijaykanwar <vijay.kanwar@synergy.net.au>
2021-05-16 22:53:13 +0800

    add code

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log('Vijay singh kanwar ')
+function addValue (num1: number, num2:number, resultOut:boolean, resultStart:string){
+    let result = num1+num2
+   if(resultOut){
+       console.log(resultStart + result);
+   }else{
+     console.log(num1 + num2);  
+   }
+    
+}
+
+const number1= 9;
+const number2=1;
+const display = true;
+const displayStartPhase = 'Result is ..'
+ addValue(number1, number2, display, displayStartPhase);
+
